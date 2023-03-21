ca9593c29a0f4c59d6240b8af13f4e0627c1270b
RaoulRSV <RaoulRSV>
2023-03-21 14:47:48 +0100

    Ajoutez des fichiers projet.

---
+++ excel-blazor-add-in/Pages/Index.razor.ts
@@ -0,0 +1,27 @@
+﻿/* Copyright(c) Maarten van Stam. All rights reserved. Licensed under the MIT License. */
+
+ (window as any).lancerchaîné = async (event) => {
+        // see https://sourcegraph.com/github.com/aspnet/AspNetCore@bd65275148abc9b07a3b59797a88d485341152bf/-/blob/src/Components/Web.JS/src/Boot.Server.ts#L41:9
+
+        console.log("lancerchaîné");
+        await callStaticLocalComponentMethod();
+        console.log("nod");   
+        event.completed();
+    }
+//}
+async function callStaticLocalComponentMethod() {
+    //window.dispatchEvent(new Event('myEvent'));
+    console.log("avant");
+    try {               
+        var dato = "init";
+        dato =await DotNet.invokeMethodAsync("BlazorAddIn", "Localfunction");                                       
+        console.log("fin demarrage : " + dato);
+    }
+    catch (err) {
+        console.log();
+        console.log("erreur : " + err.message);      
+    }
+    finally {       
+        console.log("après");
+    }
+}
